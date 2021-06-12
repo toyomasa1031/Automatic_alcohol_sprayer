1dbbde2abf2868da742b4540b428a0bbab552f49
toyomasa1031 <76829611+toyomasa1031@users.noreply.github.com>
2021-06-12 18:25:17 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,11 @@
 basic.forever(function () {
     basic.showIcon(IconNames.Asleep)
-    if (pins.analogReadPin(AnalogPin.P1) > 450) {
-        for (let index = 0; index < 2; index++) {
-            basic.showIcon(IconNames.Surprised)
-            pins.servoWritePin(AnalogPin.P0, 100)
-            basic.pause(500)
-            pins.servoWritePin(AnalogPin.P0, 0)
-            basic.pause(500)
+    pins.servoWritePin(AnalogPin.P0, 0)
+    if (pins.analogReadPin(AnalogPin.P1) > 500) {
+        basic.showIcon(IconNames.Heart)
+        for (let index = 0; index < 1; index++) {
+            pins.servoWritePin(AnalogPin.P0, 80)
         }
-        basic.clearScreen()
         basic.showIcon(IconNames.Happy)
         basic.showLeds(`
             . . . . .
@@ -19,6 +16,6 @@ basic.forever(function () {
             `)
         basic.showIcon(IconNames.Happy)
     } else {
-        pins.servoWritePin(AnalogPin.P0, 0)
+        basic.showIcon(IconNames.Asleep)
     }
 })
