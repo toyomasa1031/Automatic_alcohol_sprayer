f52b356dc1d7748be090a084c77f3059ec67ecc7
toyomasa1031 <76829611+toyomasa1031@users.noreply.github.com>
2021-06-19 08:21:15 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 5; index++) {
+        pins.servoWritePin(AnalogPin.P0, 80)
+        basic.pause(1000)
+        pins.servoWritePin(AnalogPin.P0, 0)
+        basic.pause(1000)
+    }
+    music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+})
 basic.forever(function () {
     basic.showIcon(IconNames.Asleep)
     pins.servoWritePin(AnalogPin.P0, 0)
